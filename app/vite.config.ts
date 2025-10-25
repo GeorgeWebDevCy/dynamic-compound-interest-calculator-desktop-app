e016e1fac93627225168adeb990aabfc274ece8c
George Nicolaou <orionas.elite@gmail.com>
2025-10-25 20:21:52 +0300

    build fix

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // ensure assets resolve when loaded from file:// in Electron builds
   plugins: [react()],
 })
